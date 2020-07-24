cb0d021b7d9076eb7059011b064bc41a705e413b
Joanna Preston <joannappreston@gmail.com>
2020-07-24 16:52:20 -0700

    build

--- routes/token.js
+++ routes/token.js
@@ -4,15 +4,27 @@ require("dotenv").config();
 const key = process.env.PET_FINDER_API_KEY;
 const secret = process.env.PET_FINDER_API_SECRET;
 
+// router.get("/api/getToken", (req, res) => {
+//     axios({
+//         method: "POST",
+//         url: "https://api.petfinder.com/v2/oauth2/token",
+//         data: "grant_type=client_credentials&client_id=" + key + "&client_secret=" + secret,
+//         headers: {
+//             'Content-Type': 'application/x-www-form-urlencoded'
+//         }
+//     })
+//         .then(response => res.json(response.data))
+//         .catch(err => console.log(err));
+// });
 
 router.get("/api/getToken", (req, res) => {
-    axios({
-        method: "POST",
-        url: "https://api.petfinder.com/v2/oauth2/token",
-        data: "grant_type=client_credentials&client_id=" + key + "&client_secret=" + secret,
-        headers: {
-            'Content-Type': 'application/problem+json'
-        }
+
+    let data = "grant_type=client_credentials&client_id=" + key + "&client_secret=" + secret;
+    let headers = {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    axios.post("https://api.petfinder.com/v2/oauth2/token", data, {
+        headers: headers
     })
         .then(response => res.json(response.data))
         .catch(err => console.log(err));
