cb629179761fc72a949c001b7d6716c1efbc7609
Joanna Preston <joannappreston@gmail.com>
2020-07-24 12:26:04 -0700

    build

---
+++ client/build/precache-manifest.8e60d2310adffbfae2e4abf44c275649.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23acba75733c6f03c5c7a9aaac5c9c1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6bbbb6aa798ed7b6bbcb",
+    "url": "/static/css/main.8887e74e.chunk.css"
+  },
+  {
+    "revision": "42081673cdfda0c3fef6",
+    "url": "/static/js/2.aef28100.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.aef28100.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bbbb6aa798ed7b6bbcb",
+    "url": "/static/js/main.f4681d12.chunk.js"
+  },
+  {
+    "revision": "1ca56b6e53ce1dc83ae6",
+    "url": "/static/js/runtime-main.0a3c20bd.js"
+  },
+  {
+    "revision": "b103452a11238349b6ab00226185421f",
+    "url": "/static/media/buddy.b103452a.jpg"
+  },
+  {
+    "revision": "0f48ac0e898d8c8cea44956aa8bda92e",
+    "url": "/static/media/bw-dog.0f48ac0e.jpg"
+  }
+]);
